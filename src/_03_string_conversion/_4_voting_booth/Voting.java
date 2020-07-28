f5b0231a089693c63cf4e4b588a5191a56afac1e
root <root@b7ce088e21ee>
2020-07-28 23:59:01 +0000

    slippery dot, strings, roller coaster, voting

---
+++ src/_03_string_conversion/_4_voting_booth/Voting.java
@@ -0,0 +1,17 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String age = JOptionPane.showInputDialog("How old are you?");
+		int ageInt = Integer.parseInt(age);
+		
+		if (ageInt>18) {
+			JOptionPane.showInputDialog
+		}
+	}
+
+}
